c569b59272cc5c6dafb041141a33364a10175310
Raidriar796 <145519075+Raidriar796@users.noreply.github.com>
2025-11-23 00:16:26 -0800

    Project setup

---
+++ Resurfacer/Resurfacer.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using ResoniteModLoader;
+using FrooxEngine;
+using FrooxEngine.UIX;
+using Elements.Assets;
+using Elements.Core;
+
+namespace Resurfacer;
+
+public class Resurfacer : ResoniteMod
+{
+    public override string Name => "Resurfacer";
+    public override string Author => "Raidriar796";
+    public override string Version => "1.0.0";
+    public override string Link => "";
+    public static ModConfiguration? Config;
+
+    public override void OnEngineInit()
+    {
+        Harmony harmony = new("net.raidriar796.Resurfacer");
+        Config = GetConfiguration();
+        Config?.Save(true);
+        harmony.PatchAll();
+    }
+}
