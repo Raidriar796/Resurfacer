200ccef9204e0967b303edf5f4e1c294fb55b5a4
Raidriar796 <145519075+Raidriar796@users.noreply.github.com>
2025-12-20 22:10:37 -0800

    Add unset force format
    Adds a button that unchecks "ForceExactVariant" on textures.

--- Resurfacer/Resurfacer.cs
+++ Resurfacer/Resurfacer.cs
@@ -39,7 +39,7 @@ public class Resurfacer : ResoniteMod
         ResurfacerUI.LocalScale *= 0.0005f;
 
         // Initial UI generation
-        UIBuilder UI = RadiantUI_Panel.SetupPanel(ResurfacerUI, "Resurfacer", new float2(360f, 360f), true, true);
+        UIBuilder UI = RadiantUI_Panel.SetupPanel(ResurfacerUI, "Resurfacer", new float2(360f, 480f), true, true);
         RadiantUI_Constants.SetupEditorStyle(UI);
         VerticalLayout verticalLayout = UI.VerticalLayout(4f);
         UI.Style.MinHeight = 24f;
@@ -49,25 +49,28 @@ public class Resurfacer : ResoniteMod
         UI.Canvas.AcceptPhysicalTouch.Value = false;
         UI.Canvas.MarkDeveloper();
 
-        // Setup target slot field
-        ReferenceField<Slot> targetSlot = ResurfacerUI.AttachComponent<ReferenceField<Slot>>();
         UI.Text("Target Hierarchy:", true, Alignment.BottomCenter);
         UI.Next("Root");
+
+        // Setup target slot field
+        ReferenceField<Slot> targetSlot = ResurfacerUI.AttachComponent<ReferenceField<Slot>>();
         UI.Current.AttachComponent<RefEditor>().Setup(targetSlot.Reference);
+
+        UI.Spacer(24f);
 
         // Setup preferred format
         ValueField<TextureCompression> targetFormat = ResurfacerUI.AttachComponent<ValueField<TextureCompression>>();
         UI.EnumMemberEditor(targetFormat.Value);
 
-        UI.Spacer(24f);
+        // Setup buttons for texture formats
+        var setFormatButton = UI.Button("Set Preferred Format");
+        var forceFormatButton = UI.Button("Force Preferred Format");
+        var unsetForceButton = UI.Button("Unset Force Format");
 
-        // Buttons that do things maybe
-        var setFormatButton = UI.Button("Set Format");
-        var forceFormatButton = UI.Button("Force Format");
-
-        // Subscribe buttons to do things
+        // Subscribe buttons to methods
         setFormatButton.LocalPressed += SetFormat;
         forceFormatButton.LocalPressed += ForceFormat;
+        unsetForceButton.LocalPressed += UnsetForceFormat;
 
     }
 
@@ -132,6 +135,41 @@ public class Resurfacer : ResoniteMod
                                 {
                                     texture.PreferredFormat.Value = targetFormat;
                                     texture.ForceExactVariant.Value = true;
+                                }
+                            }
+                        });
+                    }
+                    worker.ForeachSyncMember<IAssetRef>(action);
+                }
+            }
+        }
+    }
+
+    private static void UnsetForceFormat(IButton sourceButton, ButtonEventData eventData)
+    {
+        Slot targetSlot = sourceButton.Slot.GetComponentInParents<ReferenceField<Slot>>().Reference;
+        TextureCompression targetFormat = sourceButton.Slot.GetComponentInParents<ValueField<TextureCompression>>().Value;
+        Action<IAssetRef> primaryAction = null!;
+        if (targetSlot == null) return;
+
+        foreach (MeshRenderer mesh in targetSlot.GetComponentsInChildren<MeshRenderer>())
+        {
+            foreach (IAssetProvider<Material> material in mesh.Materials)
+            {
+                if (material != null)
+                {
+                    Worker worker = (Component)material;
+                    Action<IAssetRef> action;
+                    if ((action = primaryAction) == null)
+                    {
+                        action = (primaryAction = delegate (IAssetRef textureRef)
+                        {
+                            StaticTexture2D texture = textureRef.Target as StaticTexture2D;
+                            if (texture != null)
+                            {
+                                if (texture.Uncompressed.Value == false)
+                                {
+                                    texture.ForceExactVariant.Value = false;
                                 }
                             }
                         });
